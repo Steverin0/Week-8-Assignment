96a0512db98299df02a51f66145ec18d36251429
Steverin0 <scabrera28@gmail.com>
2025-04-24 13:33:06 -0600

    second commit?

---
+++ index.js
@@ -0,0 +1,28 @@
+/* 
+Create a menu app as seen in this week’s video. What you create is up to you as long as it meets the 
+following requirements:
+1) Use at least one array.
+2) Use at least two classes.
+3) Your menu should have the options to create, view, and delete elements.
+*/
+
+//Recipe list for tracking my favorite foods.
+
+class Recipe {
+    constructor (name, ingredients, steps) {
+        this.name = name;
+        ingredients = [];
+        this.steps = steps;
+    }
+
+    addRecipe() {
+        
+    }
+
+}
+
+class RecipeBook { //This will hold the different recipe instances.
+    constructor () {
+
+    }
+}
